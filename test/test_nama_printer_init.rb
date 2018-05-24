c83679938060bcdfdf8d7fb7c2c1d399671da750
lourenco <lourenco.m.c.viana@gmail.com>
2018-05-23 23:42:32 -0300

    teste da criacao de objeto

---
+++ test/test_nama_printer_init.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+require_relative '../nama_printer'
+require 'test/unit'
+
+# test invalid arguments errors
+class TestArguments < Test::Unit::TestCase
+  def test_is_str_startr
+    assert_raise(ArgumentError) { NamaPrinter.new('a', 2) }
+  end
+
+  def test_is_str_endr
+    assert_raise(ArgumentError) { NamaPrinter.new(1, 'a') }
+  end
+
+  def test_0_or_lower_startr
+    assert_raise(ArgumentError) { NamaPrinter.new(0, 1) }
+  end
+
+  def test_0_or_lower_endr
+    assert_raise(ArgumentError) { NamaPrinter.new(1, -1) }
+  end
+
+  def test_startr_bigger_tham_endr
+    assert_raise(ArgumentError) { NamaPrinter.new(12, 1) }
+  end
+
+  def test_valid_arguments
+    assert_instance_of(NamaPrinter, NamaPrinter.new(1, 2))
+  end
+end
