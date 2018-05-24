c392289b5c8ba34d95dc5294986de7b34e9b8cb5
lourenco <lourenco.m.c.viana@gmail.com>
2018-05-23 23:42:47 -0300

    teste de impressao de resultado

---
+++ test/test_print.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+require_relative '../nama_printer'
+require 'test/unit'
+
+# test text_decider
+class TestPrint < Test::Unit::TestCase
+  def test_if_printed_correclty
+    assert_nothing_thrown { NamaPrinter.new.print }
+  end
+end
