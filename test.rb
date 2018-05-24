4d17edc532bd6c9b329466bb59d0c05cd8678aba
Lourenco Viana <lourenco.viana@techlead.local>
2018-05-24 09:56:12 -0300

    removido do diretorio inicial

--- test.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-require_relative './test/test_nama_printer_init'
-require_relative './test/test_text_decider'
-require_relative './test/test_make_array'
-require_relative './test/test_to_s'
-require_relative './test/test_print'
